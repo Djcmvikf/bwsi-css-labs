18d6172b4635683def6b6328fe95f314d8eeac15
Manuel R <manurinconc@gmail.com>
2026-03-21 18:34:30 -0400

    speed 5-8

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,6 +3,7 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+robot_speed = 8 # m/s
 """
 
 def main():
@@ -12,7 +13,7 @@ def main():
 
     print(f"{name}, Welcome to the CSS course!")
 
-    robot_speed = 5 # m/s
+    
 
 if __name__ == "__main__":
     main()
